8d0c4bbf167869befef3aefb80fb6d859bf6db05
PyBeginner1 <79393101+PyBeginner1@users.noreply.github.com>
2021-04-02 17:05:31 +0530

    Delete TreeDirectory.py

--- TreeDirectory.py
+++
@@ -1,8 +0,0 @@
-import os
-
-for path, dirs, files in os.walk("C:/Users/Shashvath/Pictures"):
-    print("he current folder is : " + path)
-    for dir in dirs:
-        print("Subfolder is : " + path + dir)
-    for file in files:
-        print("Folder : " + path + file)
